3fbca21e8bd632aae82fa7da1eb849fa79dfe2cc
weiweiitcast <weiwei@itcast.cn>
2019-09-26 17:25:30 +0800

    sku商品数据可视化

--- meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/apps/meiduo_admin/urls.py
@@ -19,6 +19,8 @@ from meiduo_admin.views.login_view import LoginAPIView
 from rest_framework_jwt.views import obtain_jwt_token
 from meiduo_admin.views.home_view import *
 from rest_framework.routers import SimpleRouter
+from meiduo_admin.views.user_view import *
+from meiduo_admin.views.sku_view import *
 
 urlpatterns = [
     # 手动实现登陆视图和序列化器
@@ -33,6 +35,12 @@ urlpatterns = [
     # url(r'^statistical/month_increment/$', UserMonthIncrView.as_view()),
 
     url(r'^statistical/goods_day_views/$', GoodsVisitCountView.as_view()),
+
+    url(r'^users/$', UserAPIView.as_view()),
+
+    # sku商品
+    url(r'^skus/$', SKUViewSet.as_view({'get':'list'})),
+    url(r'^skus/categories/$', SKUViewSet.as_view({'get':'categories'})),
 ]
 
 
